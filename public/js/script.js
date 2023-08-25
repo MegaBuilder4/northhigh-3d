1ea5afe69a327a2ed1b6f29b60367d2abcc9e10a
MegaBuilder4 <craftylandon@gmail.com>
2023-08-25 07:50:35 -0500

    are

--- public/js/script.js
+++ public/js/script.js
@@ -487,7 +487,7 @@ const test = () => {
 var amount = total;
 
 
-if (amount < 0.99){
+if (amount < 5.99){
     var buybtn = document.querySelector('.btn-buy');
     var text = document.querySelector('.graytext2');
 
